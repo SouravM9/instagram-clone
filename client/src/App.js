e4a1eb466665247ff106d76d028ad69519c056cc
SouravM9 <majumdars598@gmail.com>
2022-10-26 22:34:20 +0530

    Added materialize in index.html & Add React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Login from './components/screens/Login';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={
+          <div>
+            <Home />
+          </div>
+        }>
+        </Route>
+        <Route path='/login' element={
+          <div>
+            <Login />
+          </div>
+        }>
+        </Route>
+        <Route path='/profile' element={
+          <div>
+            <Profile />
+          </div>
+        }>
+        </Route>
+        <Route path='/signup' element={
+          <div>
+            <Signup />
+          </div>
+        }>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
