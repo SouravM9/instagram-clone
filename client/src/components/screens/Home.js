6224751c8f6a2e52c4eef5b36ca3be3fcb04f1ba
SouravM9 <majumdars598@gmail.com>
2022-11-01 21:36:56 +0530

    Home page dynamic data

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,49 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/allpost', {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      }
+    })
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setData(result.posts);
+      })
+  }, []);
+
   return (
     <div className="home">
+      {
+        data.map(item => {
+          return (
+            <div className="card home-card" key={item._id}>
+            <h5>{item.postedBy.name}</h5>
+              <div className="card-image">
+                <img src={item.photo} alt={item.title} />
+              </div>
+              <div className="card-content">
+                <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                <h6>{item.title}</h6>
+                <p>{item.body}</p>
+                <input type="text" placeholder="Add a comment" />
+              </div>
+            </div>
+          )
+        })
 
-      <div className="card home-card">
-        <div className="card-image">
-          <img src="https://images.pexels.com/photos/3136403/pexels-photo-3136403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Mountains" />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>favorite</i>
-          <h6>Beautiful</h6>
-          <p>Fucking Beautiful</p>
-        </div>
-      </div>
-      <div className="card home-card">
-        <div className="card-image">
-          <img src="https://images.pexels.com/photos/3136403/pexels-photo-3136403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Mountains" />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>favorite</i>
-          <h6>Beautiful</h6>
-          <p>Fucking Beautiful</p>
-        </div>
-      </div>
-      <div className="card home-card">
-        <div className="card-image">
-          <img src="https://images.pexels.com/photos/3136403/pexels-photo-3136403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Mountains" />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>favorite</i>
-          <h6>Beautiful</h6>
-          <p>Fucking Beautiful</p>
-        </div>
-      </div>
-      <div className="card home-card">
-        <div className="card-image">
-          <img src="https://images.pexels.com/photos/3136403/pexels-photo-3136403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Mountains" />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>favorite</i>
-          <h6>Beautiful</h6>
-          <p>Fucking Beautiful</p>
-        </div>
-      </div>
+      }
+
     </div>
   )
 }
