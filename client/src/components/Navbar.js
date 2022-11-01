674673ea1846eea4c19b15008d3a0e47e38d678d
SouravM9 <majumdars598@gmail.com>
2022-11-01 21:05:04 +0530

    useContext done on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
 
 const Navbar = () => {
+    const { state, dispatch } = useContext(UserContext);
+
+    // renderList is allowing user to access Posts only if valid user is logged in
+    const renderList = () => {
+        if (state) {    // User is logged in, then 
+            return [
+                <>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/createpost">Create Post</Link></li>
+                </>
+            ]
+        }
+        else {
+            return [
+                <>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                </>
+            ]
+        }
+    }
     return (
         <nav>
             <div className="nav-wrapper white">
-                <Link to="/" className="brand-logo left">Instagram</Link>
+                {/* Same list render list i.e., if logged in then only you can go to home screen */}
+                <Link to={state ? "/" : "/login"} className="brand-logo left">Instagram</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/createpost">Create Post</Link></li>
+                    {renderList()}
                 </ul>
             </div>
         </nav>
