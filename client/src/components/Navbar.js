fbfaaf61febc078f75b6546a0c9170887e8441e3
SouravM9 <majumdars598@gmail.com>
2022-11-01 21:55:23 +0530

    logout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 
 const Navbar = () => {
     const { state, dispatch } = useContext(UserContext);
-
+    const navigate = useNavigate();
     // renderList is allowing user to access Posts only if valid user is logged in
     const renderList = () => {
         if (state) {    // User is logged in, then 
@@ -12,6 +12,17 @@ const Navbar = () => {
                 <>
                     <li><Link to="/profile">Profile</Link></li>
                     <li><Link to="/createpost">Create Post</Link></li>
+                    <li>
+                        <button className="btn waves-effect waves-light #ec407a pink lighten-1"
+                            onClick={() => {
+                                localStorage.clear();
+                                dispatch({ type: "CLEAR" })
+                                navigate('/login');
+                            }}
+                        >
+                            Logout
+                        </button>
+                    </li>
                 </>
             ]
         }
