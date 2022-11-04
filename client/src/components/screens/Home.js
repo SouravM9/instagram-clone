48473bcf12573f2dc6de8db16435c7b8c8f36788
SouravM9 <majumdars598@gmail.com>
2022-11-04 22:46:02 +0530

    Delete post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -101,13 +101,44 @@ const Home = () => {
     setComment('');  // Clear the comment
   }
 
+  const deletePost = (postid) => {
+    fetch(`/deletepost/${postid}`, {
+      method: "delete",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+      .then(result => {
+        // console.log(result)
+        // Filtering the data & not including the deleted one
+        const newData = data.filter(item => {
+          return item._id !== result._id
+        })
+        setData(newData)
+      })
+  }
+
   return (
     <div className="home">
       {
         data.map(item => {
           return (
             <div className="card home-card" key={item._id}>
-              <h5>{item.postedBy.name}</h5>
+
+              <h5 style={{ padding: "5px" }}>
+                <Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"}>
+                  {item.postedBy.name}
+                </Link>
+
+                {item.postedBy._id == state._id
+                  && <i className="material-icons" style={{
+                    float: "right"
+                  }}
+                    onClick={() => deletePost(item._id)}
+                  >delete</i>
+
+                }</h5>
+
               <div className="card-image">
                 <img src={item.photo} alt={item.title} />
               </div>
